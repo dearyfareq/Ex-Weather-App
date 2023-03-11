b3e4ba9bdad70fa3223ed0a4a1d0cf1887bb05a9
Diyari <dearyfareq15@gmail.com>
2023-03-11 14:02:25 +0300

    added 3D background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
+import Input from "./components/input";
+import "./App.scss"
 function App() {
   return (
     <div>
-      <h1>
-        test
-      </h1>
-    </div>
-  );
+<div id="stars-container">
+			<div id='stars'></div>
+			<div id='stars2'></div>
+			<div id='stars3'></div>
+</div>
+<Input />
+</div>);
 }
 
 export default App;
